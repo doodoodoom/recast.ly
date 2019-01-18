b2410cd00d1def40b415625ef399a60b554ce568
doodoodoom <madridmatt17@gmail.com>
2019-01-17 20:21:11 -0800

    Implement searchYoutube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var url = 'https://www.googleapis.com/youtube/v3/search';
+  $.ajax({
+    url: url,
+    type: 'GET',
+    data: {key: options.key,
+      q: options.query,
+      type: 'video',
+      maxResults: options.max,
+      videoEmbeddable: true,
+      part: 'snippet'
+    },
+    contentType: 'application/json',
+    success: function(data) {
+      callback(data.items);
+    },
+    error: function(data) {
+      console.log('there was an issue getting this ', data);
+    }
+  });
 };
+// A function to be called if the request succeeds. The function gets passed three arguments:
 
 export default searchYouTube;
